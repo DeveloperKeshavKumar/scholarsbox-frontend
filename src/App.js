7731d749acd9076a90da0a8e75cb328ff1f0c3ba
DeveloperKeshavKumar <keshav0kumar@hotmail.com>
2024-03-12 11:09:42 +0530

    Unnecessary files removed and necessary files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage';
+import Dashboard from './pages/Dashboard'
+import Userdetails from './pages/Userdetails'
+import Projects from './pages/Projects';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Outlet />}>
+          <Route index element={<Homepage />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/user' element={<Userdetails />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='*' element={<div className='text-4xl mt-16 tracking-wider text-blue-600'>404 Not Found</div>} />
+        </Route>
+
+      </Routes>
     </div>
   );
 }
