30e3264e64b7504f1dececbb327b6645fbb12a61
Keshav Kumar <keshav0kumar@hotmail.com>
2024-04-15 02:02:41 +0530

    Made route open for all

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,17 +2,19 @@ import { Navbar } from "../components/Navbar";
 import ProjectTable from "../components/ProjectTable";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Projects = () => {
+  const {token} = useSelector((state)=>state.auth)
   return (
     <>
       <Navbar />
       <div className="w-11/12 max-w-5xl mx-auto mt-10">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-left ">All Projects</h1>
-          <Link to="/projects/create">
+          {token && <Link to="/projects/create">
             <button className="mt-5 rounded-[8px] bg-blue-600 py-[8px] px-[12px] font-medium text-white" >Create Project</button>
-          </Link>
+          </Link>}
         </div>
         <p className="text-left font-normal mt-2 text-gray-500">Explore all projects and find one for you</p>
       </div>
